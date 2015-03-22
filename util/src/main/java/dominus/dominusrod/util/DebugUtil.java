888947b7b2e66481942550707cd6a7920caeb70b
guoxu1231 <guoxu1231@gmail.com>
2015-03-22 22:55:14 +0800

    util module initil commit

--- util/src/main/java/dominus/dominusrod/util/DebugUtil.java
+++ util/src/main/java/dominus/dominusrod/util/DebugUtil.java
@@ -1,10 +1,15 @@
 package dominus.dominusrod.util;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
+import android.widget.Toast;
 
 /**
+ * Debug Utility
+ * <p/>
  * Created by shawguo on 3/22/15.
  */
 public abstract class DebugUtil {
@@ -13,12 +18,49 @@ public abstract class DebugUtil {
 
         Log.d(TAG, intent.toString());
 
-        Bundle bundle = intent.getExtras();
-        for (String key : bundle.keySet()) {
-            Object value = bundle.get(key);
-            Log.d(TAG, String.format("\t%s %s (%s)", key,
-                    value.toString(), value.getClass().getName()));
+        Bundle extras = intent.getExtras();
+        if (extras != null) {
+            for (String key : extras.keySet()) {
+                Object value = extras.get(key);
+                Log.d(TAG, String.format("\t%s %s (%s)", key,
+                        value.toString(), value.getClass().getName()));
+            }
+        } else {
+            Log.d(TAG, "no extras");
+
         }
+    }
+
+    /**
+     * Toast message on the main thread.
+     * <p/>
+     * <blockquote > you can only use Toast in the main GUI thread, or else you
+     * run into problems where the Toast message doesn't disappear after a
+     * period (because the main GUI context doesn't know anything about Toast
+     * messages used in a separate thread context)
+     * <p/>
+     * Android: How can i show a toast from a thread running in a remote
+     * service?
+     * http://stackoverflow.com/questions/6134013/android-how-can-i-show
+     * -a-toast-from-a-thread-running-in-a-remote-service </blockquote >
+     *
+     * @param context
+     * @param text
+     */
+    public static void toast(final Context context, final CharSequence text) {
+
+        // Toast.makeText(context, text, Toast.LENGTH_SHORT).show();
+        Log.i(context.getPackageName(), text.toString());
+
+        Handler h = new Handler(context.getMainLooper());
+
+        h.post(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(context, text, Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 
 
